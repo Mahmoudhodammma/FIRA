815b0a7b28a63e19e85f49d159e19e17abff5dab
Mahmoudhodammma <91755885+Mahmoudhodammma@users.noreply.github.com>
2022-02-08 02:10:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,41 +2,13 @@ import requests
 import flask
 from flask import *
 app = Flask(__name__)
-@app.route("/Api/Fira/id=<id>/user=<user>/sessionid=<ses>")
-def ue(id,user):
-	url = "http://185.231.59.247/xfirax/"
-	data = {f'inscoo':'Cookie: mid=YF55GAALAAF55lDR3NkHNG4S-vjw; ig_did=F3A1F3B5-01DB-457B-A6FA-6F83AD1717DE; ig_nrcb=1; shbid=13126; shbts=1616804137.1316793; rur=PRN; ig_direct_region_hint=ATN; csrftoken=ot7HDQ6ZX2EPbVQe1P9Nqvm1WmMkzKn2; ds_user_id=46165248972; sessionid='+ses,
- 'pkx':id,
- 'uname':user,
- 'submit':'submit'}
-	info = requests.post(url,data=data).text
-	like=str(info.split('Like Coin- :')[1])
-	likecoin=like.split('<br>')[0]
-	follow=str(info.split('Follow Coin- :')[1])
-	followcoin=follow.split('\n')[0]
-	if 'Status- : ok' in info:
-		de = {
-	"Telegram":"@uufffuu",
-	"Like Coin":likecoin,
-	"Follow Coin":followcoin,
-	"Status":"True"
-	}
-		return de
-	if 'Status- : nok' in info:
-		de = {
-	"Telegram":"@uufffuu",
-	"Like Coin":likecoin,
-	"Follow Coin":followcoin,
-	"Status":"False"
-	}
-		return de
+@app.route("/check/tele/")
+def ue():
+	User = request.args.get("User")
+	re = requests.get(f"https://t.me/{User}").text
+	if "tgme_username_link" in re:
+		return jsonify(check=True,Telegram="@uufffuu",User=User)
 	else:
-		de = {
-	"Telegram":"@uufffuu",
-	"Like Coin":likecoin,
-	"Follow Coin":followcoin,
-	"Status":"False"
-	}
-		return de
+		return jsonify(check=False,Telegram="@uufffuu",User=User)
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run()
